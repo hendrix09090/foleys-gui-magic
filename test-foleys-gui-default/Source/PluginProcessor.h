174c9eb0267bef4321e08fe0f3a2df1df3efe5ee
hendrix09090 <hendrix0909@gmail.com>
2025-03-12 11:26:56 +0800

    Add files via upload

---
+++ test-foleys-gui-default/Source/PluginProcessor.h
@@ -0,0 +1,52 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include <foleys_gui_magic/General/foleys_MagicProcessor.h>
+
+//==============================================================================
+/**
+*/
+class TestAudioProcessor  : public foleys::MagicProcessor
+{
+public:
+    //==============================================================================
+    TestAudioProcessor();
+    ~TestAudioProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+private:
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TestAudioProcessor)
+};
